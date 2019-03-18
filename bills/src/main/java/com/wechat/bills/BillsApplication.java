aa8a812fbd3a071d5f40227ddf60d1505ff13487
shore0549 <shore0549@gmail.com>
2019-03-18 17:56:25 +0800

    单线程定时器变成多线程定时器的关键点： 1，xml配置 2，xml必须在IDEA的配置项project structure-modules 里面添加， 仅仅在springboot里面添加注解importResource是不够的

--- bills/src/main/java/com/wechat/bills/BillsApplication.java
+++ bills/src/main/java/com/wechat/bills/BillsApplication.java
@@ -3,13 +3,15 @@ package com.wechat.bills;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication(scanBasePackages = {"com.wechat.bills", "com.wechat.bills"})
-//@ImportResource(locations={"classpath:application-tast.xml"})
+@ImportResource(locations={"classpath:application-tast.xml"})
 //@MapperScan("com.young.mapper")//必须加这个，不加报错，如果不加，也可以在每个mapper上添加@Mapper注释
 //@EnableScheduling
 public class BillsApplication {
